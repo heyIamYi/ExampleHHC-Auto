a8b1d591562e102c58c97776dfb4f3c355b25406
hetRich <95585077+heyIamYi@users.noreply.github.com>
2022-06-17 14:04:22 +0800

    12321321

---
+++ js/navbar.js
@@ -0,0 +1,114 @@
+var hoverbox = document.querySelectorAll('.downlist');
+hoverbox.forEach(element => {
+    var nav = document.getElementById('nav');
+    element.addEventListener('mousemove', function () {
+        nav.classList.add('blue');
+    });
+    nav.addEventListener('mouseleave', function () {
+        nav.classList.remove('blue');
+    });
+});
+var bgli = document.getElementById('bglist');
+var focuss = document.getElementById('focus');
+var focusl = document.getElementById('focuslogo');
+var focusm = document.getElementById('focusmain');
+var xx = document.getElementById('XX');
+var entry = document.getElementById('focus-button');
+var focusbutton = document.getElementById('focus-button');
+focusbutton.addEventListener('mousemove',function(){
+    focusbutton.innerHTML = `<h1>E n t r y</h1>
+    <img src="./image/blue_white_entry.svg" alt="">`
+})
+focusbutton.addEventListener('mouseleave',function(){
+    focusbutton.innerHTML = `<h1>E n t r y</h1>
+    <img src="./image/right_white_entry.svg" alt="">`
+})
+bgli.addEventListener('click', function () {
+    focuss.classList.remove('none')
+    focusl.classList.remove('none')
+    focusm.classList.remove('none')
+})
+function xxlist(){
+    focuss.classList.add('none')
+    focusl.classList.add('none')
+    focusm.classList.add('none')
+}
+xx.addEventListener('click', xxlist);
+entry.addEventListener('click', xxlist);
+
+
+var el = document.querySelector('body');
+var nav = document.getElementById('nav');
+var subnav = document.getElementById('subnav');
+var minilogo = document.getElementById('minilogo');
+var biglogo = document.getElementById('biglogo');
+var bgrlist = document.querySelectorAll('.stuffing');
+var whitelogo = document.getElementById('white-logo');
+var banner = document.querySelector('#banner');
+el.addEventListener('touchmove', show, false);
+el.addEventListener('mousewheel', show, false);
+el.addEventListener('DOMMouseScroll', show, false);
+show(), false;
+function navin() {
+    var windowtop = document.documentElement.scrollTop;
+    // console.log(windowtop);
+    if (window.innerWidth > 993) {
+        if (windowtop > 100) {
+            // console.log('111');
+            subnav.classList.add("none")
+            biglogo.classList.add('none')
+        } else {
+            subnav.classList.remove("none")
+            biglogo.classList.remove('none')
+        }
+    } else {
+        whitelogo.classList.remove('none')
+        if (windowtop > banner.clientHeight) {
+            whitelogo.classList.add('none')
+            minilogo.style.display = 'block'
+            bgrlist[1].style.backgroundColor = '#0146a8';
+            bgrlist[0].style.backgroundColor = '#0146a8';
+        } else {
+            whitelogo.classList.remove('none');
+            minilogo.style.display = 'none'
+            bgrlist[1].style.backgroundColor = 'white';
+            bgrlist[0].style.backgroundColor = 'white';
+        }
+    }
+}
+
+function show(event) {
+    var wi = window.innerHeight //頁面高度
+    navin();
+    window.onscroll = () => {
+        navin();
+        let cH = document.documentElement.clientHeight;
+        let sH = document.documentElement.scrollHeight;
+        let sT = document.documentElement.scrollTop;
+        // console.log(cH, sH, sT);
+    }
+}
+
+
+
+hoverbox.forEach(element => {
+    var nav = document.getElementById('nav');
+    element.addEventListener('mousemove', function () {
+        nav.classList.add('blue');
+        whitelogo.classList.remove('none');
+        minilogo.classList.add('none');
+        biglogo.classList.add('none');
+        fons.forEach(element => {
+            element.style.color='white'
+        });
+    });
+    element.addEventListener('mouseleave', function () {
+        nav.classList.remove('blue');
+        whitelogo.classList.add('none');
+        minilogo.classList.remove('none');
+        biglogo.classList.remove('none');
+        fons.forEach(element => {
+            element.style.color='#B4B4B4'
+        });
+    });
+});
